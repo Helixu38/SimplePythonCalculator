901058c12555cfb3457b3ca7f88bab17036cad9c
Helixu38 <vipase2004@gmail.com>
2023-08-12 12:57:59 +0700

    Have clear function . Added error handling

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,50 @@
-operator = input("Enter an operator(+ - * /): ")
-num1 = float(input("Enter the first number : "))
-num2 = float(input("Enter the second number: "))
 
-if operator == "+":
-    result = num1 + num2
-    print(round(result,3))
-elif operator == "-":
-    result = num1 - num2
-    print(round(result,3))
-elif operator == "*":
-    result = num1 * num2
-    print(round(result,3))    
-elif operator == "/":
-    result = num1 / num2
-    print(round(result,3))
-else:
-    print(f"{operator} is not a valid operator")
+
+# Function for operator
+def add(x,y):
+    return x + y
+def negative(x,y):
+    return x - y
+def multiple(x,y):
+    return x * y
+def divide(x,y):
+    return x / y  
+
+# User input operator
+print("Operator ------")
+command = """
+1 for +
+2 for -
+3 for *
+4 for /
+"""
+print(command)
+while True:
+
+    operator = input("Please enter the number based on the operator you want : ")
+
+
+
+#Check for user input + Error handling
+    if operator in ('1','2','3','4'):
+        try:
+            x = float(input("Please enter the first number : "))
+            y = float(input("Please enter the second number : "))
+        except ValueError:
+            print("Invalid input. Please enter a number")
+            continue
+
+        if operator == "1":
+            print(x , "+" , y , "=" , add(x,y))
+        elif operator == "2":
+            print(x , "-" , y , "=" , negative(x,y))
+        elif operator == "3":
+            print(x , "*" , y , "=" , multiple(x,y))
+        elif operator == "4":
+            print(x , "/" , y , "=" , divide(x,y))
+        
+        next_calculation = input("Do you want to do another calculation ? (yes or no) : ")
+        if next_calculation == "no":
+            break
+        else:
+            print("Invalid input")
